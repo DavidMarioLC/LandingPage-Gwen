1f837c9f62909d4491415c4ab7edce76b9111a23
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-07-08 22:53:10 -0500

    project finish

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
 const menuIcon = document.querySelector('#menu-icon');
 const menu = document.querySelector('#menu');
 const stickMenu = document.querySelector('.menu-hamburguer-icon')
+const listItemMenu = document.querySelectorAll('.list-link');
+
 
 function showMenu(){
-    menu.classList.toggle('showMenu');
-    stickMenu.classList.toggle('menuIconAnimate');
+    menu.classList.remove('hideMenu');
+    menu.classList.add('showMenu');
+    stickMenu.classList.add('menuIconAnimate');
+     stickMenu.classList.remove('menuIconAnimateHide');
 }
 
+function changeFocus(){
+    listItemMenu.forEach(item=> item.classList.remove('isActive'))
+    this.classList.add('isActive');
+
+    menu.classList.remove('showMenu');
+    menu.classList.add('hideMenu');
+    stickMenu.classList.remove('menuIconAnimate');
+    stickMenu.classList.add('menuIconAnimateHide');
+}
 
 menuIcon.addEventListener('click',showMenu);
+listItemMenu.forEach(item=> item.addEventListener('click',changeFocus))
